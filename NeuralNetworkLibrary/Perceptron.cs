d708eb6aa3d87fc7da316aa47ce7be81d642f973
Mason Lee <asonmay09@gmail.com>
2024-09-11 18:26:41 -0700

    continued working on gradient descent, needs testing

--- NeuralNetworkLibrary/Perceptron.cs
+++ NeuralNetworkLibrary/Perceptron.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace NeuralNetworkLibrary
 {
@@ -34,7 +36,7 @@ namespace NeuralNetworkLibrary
             for(int i = 0; i < Weights.Length; i++) Weights[i] = random.Next(min, max);
         }
 
-        private (double[], double) Mutate()
+        private (double[], double) MutateHillClimber()
         {
             double[] newWeights = new double[Weights.Length];
             Weights.CopyTo(newWeights, 0);
@@ -84,7 +86,7 @@ namespace NeuralNetworkLibrary
 
         public double TrainWithHillClimbing(double[][] inputs, double[] desiredOutputs, double currentError)
         {
-            (double[], double) mutated = Mutate();
+            (double[], double) mutated = MutateHillClimber();
             double mutatedError = GetError(inputs, desiredOutputs, mutated.Item1, mutated.Item2);
 
             if(mutatedError < currentError || currentError == 0)
@@ -97,19 +99,88 @@ namespace NeuralNetworkLibrary
             return currentError;
         }
 
-        private double CalculatePartiaDerivative()
+        private double CalculateWeightPartiaDerivative(int index, double[] inputs, double desiredOutputs)
         {
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++) sum += inputs[i] * Weights[i];
 
+            return errorFunc.Derivative(Compute(inputs, Weights, Bias), desiredOutputs) + activationFunction.Derivative(sum + Bias) + inputs[index];
         }
 
-        public double TrainWithGradientDescent(double[] inputs, double desiredOutputs)
+        private double CalculateBiasPartialDerivitive(double[] inputs, double desiredOutputs)
         {
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++) sum += inputs[i] * Weights[i];
 
+            return errorFunc.Derivative(Compute(inputs, Weights, Bias), desiredOutputs) + activationFunction.Derivative(sum + Bias);
         }
 
-        public double TrainWithGradientDescent(double[][] inputs, double desiredOutputs)
+        private (double[], double) GetMutateValues(double[] inputs, double desiredOutput)
         {
+            double[] WeightValues = new double[Weights.Length];
+            double BiasValue;
 
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                WeightValues[i] = weightMutationAmount * -CalculateWeightPartiaDerivative(i, inputs, desiredOutput);
+            }
+            BiasValue = biasMutationAmount * -CalculateBiasPartialDerivitive(inputs, desiredOutput);
+
+            return (WeightValues, BiasValue);
+        }
+
+        private (double[], double) GetBachMutateValues(double[][] inputs, double[] desiredOutputs)
+        {
+            (double[], double)[] mutateValues = new (double[], double)[inputs.GetLength(1)];
+            for (int i = 0; i < inputs.GetLength(1); i++)
+            {
+                mutateValues[i] = GetMutateValues(inputs[i], desiredOutputs[i]);
+            }
+            double[] weightMutateValues = new double[Weights.Length];
+            double biasMutateValue = 0;
+
+            for (int i = 0; i < inputs.GetLength(1); i++)
+            {
+                for (int j = 0; j < inputs.GetLength(0); j++)
+                {
+                    weightMutateValues[i] += mutateValues[i].Item1[j];
+                }
+                biasMutateValue += mutateValues[i].Item2;
+            }
+
+            return (weightMutateValues, biasMutateValue);
+        }
+
+        private (double[], double) Mutate(double[] weightMutationAmounts, double biasMutationAmount)
+        {
+            double[] newWeights = new double[Weights.Length];
+            Weights.CopyTo(newWeights, 0);
+            double newBias = Bias;
+
+            for(int i = 0; i < Weights.Length;i++)
+            {
+                newWeights[i] += weightMutationAmounts[i];
+            }
+            newBias += biasMutationAmount;
+
+            return (newWeights, newBias);
+        }
+
+        public double TrainWithGradientDescent(double[][] inputs, double[] desiredOutputs, double currentError)
+        {
+            (double[], double) mutationAmounts = GetBachMutateValues(inputs, desiredOutputs);
+            (double[], double) mutated = Mutate(mutationAmounts.Item1, mutationAmounts.Item2);
+
+            double mutatedError = GetError(inputs, desiredOutputs, mutated.Item1, mutated.Item2);
+
+            if (mutatedError < currentError || currentError == 0)
+            {
+                Weights = mutated.Item1;
+                Bias = mutated.Item2;
+                return mutatedError;
+            }
+
+            return currentError;
         }
     }
 }
